2b85171081d6c65c31ad883da7a3ea6cbdacb009
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-02-13 15:54:35 +0900

    色変更機能を作成

---
+++ PerpetualMotion/Assets/Script/Base/ColorInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorInfo : MonoBehaviour
+{
+    public COLOR_TYPE colorType;
+    public enum COLOR_TYPE 
+    {
+        White,
+        Black,
+        Red,
+        Bulu,
+        Yellow,
+        Green
+    }
+    
+    public Color SelectColor(COLOR_TYPE colorType)
+    {
+        Color color = new Color(255, 255, 255);
+        switch (colorType)
+        {
+            case COLOR_TYPE.White:
+                color = new Color(255, 255, 255);
+                break;
+            case COLOR_TYPE.Black:
+                color = new Color(0, 0, 0);
+                break;
+            case COLOR_TYPE.Red :
+                color = new Color(255, 0, 0);
+                break;
+            case COLOR_TYPE.Bulu:
+                color = new Color(0, 255, 0);
+                break;
+            case COLOR_TYPE.Yellow:
+                color = new Color(255, 255, 0);
+                break;
+            case COLOR_TYPE.Green:
+                color = new Color(0, 0, 255);
+                break;
+        }
+
+        return color;
+    }
+}
