6e498f4ee437f47abaf45e7817ca72f0323c387b
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-04-02 11:14:57 +0900

    説明を追加

--- PerpetualMotion/Assets/Script/Enemy/EnemyColliderController.cs
+++ PerpetualMotion/Assets/Script/Enemy/EnemyColliderController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ここは実際に見えるEnemyの実体です。Enemy側で衝突判定を取る場合はここに書いてください
+/// </summary>
 public class EnemyColliderController : MonoBehaviour
 {
+    /// <summary>生成時に渡されるコントローラー</summary>
     public EnemyController enemyController;
     // Start is called before the first frame update
     void Start()
