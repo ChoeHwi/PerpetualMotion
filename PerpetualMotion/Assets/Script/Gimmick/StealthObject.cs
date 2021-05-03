8c88b73a191a0468679d323fc9f5052169a51381
mypace-hyou <hyou290101@gmail.com>
2021-05-03 14:36:09 +0900

    プレイヤーと小物が融合した時に小物の上にプレイヤーの目を表示されるようにした

--- PerpetualMotion/Assets/Script/Gimmick/StealthObject.cs
+++ PerpetualMotion/Assets/Script/Gimmick/StealthObject.cs
@@ -7,13 +7,31 @@ public class StealthObject : ColorInfo
     [Header("このオブジェクトの色")]
     public COLOR_TYPE nowColor = COLOR_TYPE.Blank;
     SpriteRenderer objImage;
+    public GameObject eyes;
+    PlayerController p_con;
 
     private void Start()
     {
+        p_con = FindObjectOfType<PlayerController>();
         objImage = GetComponent<SpriteRenderer>();
         Form_Color(nowColor);
+        eyes.gameObject.SetActive(false);
+        Instantiate(eyes, this.transform);
     }
-
+    public void EyeController_Tr()
+    {
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(true);
+        }
+    }
+    public void EyeController_Fa()
+    {
+        foreach (Transform item in transform)
+        {
+            item.gameObject.SetActive(false);
+        }
+    }
     public void Form_Color(COLOR_TYPE color)
     {
         nowColor = color;
