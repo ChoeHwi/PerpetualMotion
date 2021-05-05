5841bb4d2064113978be559bea4bbcbb85aa446c
mypace-hyou <hyou290101@gmail.com>
2021-05-05 13:44:48 +0900

    ゲームオーバー画面のスプライトを変更した。ボタンをイメージに変更した

---
+++ PerpetualMotion/Assets/Script/UI/Event.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Event : MonoBehaviour
+{
+    GameManager gm;
+    public Sprite[] Sprit_UI;
+    public string LoadScene_name;
+
+
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+    public void EnterButton()
+    {
+        GetComponent<Image>().sprite = Sprit_UI[0];
+    }
+    public void ExitButton()
+    {
+        GetComponent<Image>().sprite = Sprit_UI[1];
+    }
+    public void DownButton()
+    {
+        gm.LoadScene(LoadScene_name);
+    }
+}
