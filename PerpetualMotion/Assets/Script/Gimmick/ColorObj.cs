bdd0cccd261a186dd90513368bb172c3fff6b055
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-05-05 15:09:11 +0900

    オブジェクト修正

---
+++ PerpetualMotion/Assets/Script/Gimmick/ColorObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorObj : ColorInfo
+{
+    [Header("このオブジェクトの色")]
+    public COLOR_TYPE nowColor = COLOR_TYPE.Blank;
+    SpriteRenderer objImage;
+
+    private void Start()
+    {
+        objImage = GetComponent<SpriteRenderer>();
+        Form_Color(nowColor);
+    }
+
+    public void Form_Color(COLOR_TYPE color)
+    {
+        nowColor = color;
+        objImage.sprite = SelectColor(nowColor)[0];
+    }
+}
