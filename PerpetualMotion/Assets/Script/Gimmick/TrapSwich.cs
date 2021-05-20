1a986bcf4f26c48ee3350eb6244f32362d7d132e
K_Arai00 <akgame36@gmail.com>
2021-05-20 14:15:40 +0900

    不要な部分の削除

--- PerpetualMotion/Assets/Script/Gimmick/TrapSwich.cs
+++ PerpetualMotion/Assets/Script/Gimmick/TrapSwich.cs
@@ -2,22 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary> トラップを作動させるクラス </summary>
 public class TrapSwich : MonoBehaviour
 {
     [Header("対応する電流トラップ")]
     [SerializeField] ElectricTrap[] electricTrap;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void TrapActuation()
     {
