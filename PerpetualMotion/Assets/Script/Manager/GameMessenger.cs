97decf286348f43a50488f93dadcda066f95b8ee
K_Arai00 <akgame36@gmail.com>
2021-05-24 00:53:56 +0900

    追加した

--- PerpetualMotion/Assets/Script/Manager/GameMessenger.cs
+++ PerpetualMotion/Assets/Script/Manager/GameMessenger.cs
@@ -10,7 +10,11 @@ public class GameMessenger : MonoBehaviour
     public GameObject gameOverResult = null;
     /// <summary>台座のUI</summary>
     public GameObject mobius;
+    /// <summary> キャンバス下にあるMobiusUIオブジェクト </summary>
     public Mobius mobiusScript;
     /// <summary> 参照先のクラスの変数 </summary>
+    public GameObject clearAnim = null;
+    /// <summary> 参照先のクラスの変数 </summary>
     public GameObject director;
+    
 }
