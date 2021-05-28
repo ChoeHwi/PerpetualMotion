a7319e3c3f2e6798c2cc240d7326c6aa1983f5fc
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-05-28 16:21:41 +0900

    パレット編集

--- PerpetualMotion/Assets/Script/Stage/StageGenerator.cs
+++ PerpetualMotion/Assets/Script/Stage/StageGenerator.cs
@@ -6,16 +6,22 @@ using UnityEngine.Tilemaps;
 public class StageGenerator : MonoBehaviour
 {
     [SerializeField]
-    Grid grid;
+    Grid m_grid;
     /// <summary>床のタイルマップ</summary>
-    Tilemap floorTilemap;
+    Tilemap m_floorTilemap;
     /// <summary>壁のタイルマップ</summary>
-    Tilemap wallTilemap;
+    Tilemap m_wallTilemap;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3Int position1z0 = new Vector3Int(-4, -4, 0);
+        Tile floorTile = Resources.Load<Tile>("Pelette/Tiles/build_00");
+        m_floorTilemap.SetTile(position1z0, floorTile);
+
+        Vector3Int position1z1 = new Vector3Int(-4, -4, 1);
+        Tile wallTile = Resources.Load<Tile>("Pelette/Tiles/roof_00");
+        m_wallTilemap.SetTile(position1z1, wallTile);
     }
 
     // Update is called once per frame
