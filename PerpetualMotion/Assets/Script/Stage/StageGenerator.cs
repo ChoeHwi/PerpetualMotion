3fba835ae99bc5cc5787baedaf6683c9f84a13fc
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-05-28 14:47:23 +0900

    不要なパレットを削除

---
+++ PerpetualMotion/Assets/Script/Stage/StageGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class StageGenerator : MonoBehaviour
+{
+    [SerializeField]
+    Grid grid;
+    /// <summary>床のタイルマップ</summary>
+    Tilemap floorTilemap;
+    /// <summary>壁のタイルマップ</summary>
+    Tilemap wallTilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
