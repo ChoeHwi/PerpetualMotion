f9f309f0666bbf5b4f214e1da382ac9525d55483
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-05-30 12:05:44 +0900

    選択中の色にアウトラインを追加

--- PerpetualMotion/Assets/Script/UI/ChengColor.cs
+++ PerpetualMotion/Assets/Script/UI/ChengColor.cs
@@ -7,6 +7,7 @@ public class ChengColor : MonoBehaviour
 {
     public int selectNumber = 0;
     ColorSelection colorSelection;
+    [SerializeField] GameObject[] selecttingBacks;
     void Start()
     {
         colorSelection = FindObjectOfType<ColorSelection>();
@@ -16,18 +17,34 @@ public class ChengColor : MonoBehaviour
     {
         if (InputChanger.InputInform(InputChanger.INPUTKEY_TYPE.UpSelect))
         {
+            selecttingBacks[0].SetActive(true);
+            selecttingBacks[1].SetActive(false);
+            selecttingBacks[2].SetActive(false);
+            selecttingBacks[3].SetActive(false);
             selectNumber = 0;
         }
         if (InputChanger.InputInform(InputChanger.INPUTKEY_TYPE.DownSelect))
         {
+            selecttingBacks[0].SetActive(false);
+            selecttingBacks[1].SetActive(false);
+            selecttingBacks[2].SetActive(true);
+            selecttingBacks[3].SetActive(false);
             selectNumber = 2;
         }
         if (InputChanger.InputInform(InputChanger.INPUTKEY_TYPE.LeftSelect))
         {
+            selecttingBacks[0].SetActive(false);
+            selecttingBacks[1].SetActive(false);
+            selecttingBacks[2].SetActive(false);
+            selecttingBacks[3].SetActive(true);
             selectNumber = 3;
         }
         if (InputChanger.InputInform(InputChanger.INPUTKEY_TYPE.RightSelect))
         {
+            selecttingBacks[0].SetActive(false);
+            selecttingBacks[1].SetActive(true);
+            selecttingBacks[2].SetActive(false);
+            selecttingBacks[3].SetActive(false);
             selectNumber = 1;
         }
     }
