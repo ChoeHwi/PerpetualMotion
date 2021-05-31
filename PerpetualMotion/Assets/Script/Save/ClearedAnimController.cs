b293a104c488529b931dc844a54ecffbbaf73dea
DESKTOP-7O9R4I6\79238 <hwi26261@gmail.com>
2021-05-31 17:37:22 +0900

    細かな調整

--- PerpetualMotion/Assets/Script/Save/ClearedAnimController.cs
+++ PerpetualMotion/Assets/Script/Save/ClearedAnimController.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class ClearedAnimController : MonoBehaviour
 {
     SaveManager m_saveManager;
-    [SerializeField] public int m_stageNumber;
-    [SerializeField] GameObject m_clearedAnim;
-    bool m_isClear;
+    [SerializeField] GameObject[] m_clearedAnim;
     void Start()
     {
         if (GameObject.FindObjectOfType<SaveManager>())
         {
             m_saveManager = GameObject.FindObjectOfType<SaveManager>();
-            m_isClear = m_saveManager.saveData.m_clearedStages[m_stageNumber - 1];
-            if (m_isClear)
+            
+            for(int i = 0; i < m_clearedAnim.Length; i++)
             {
-                m_clearedAnim.SetActive(true);
-            }
-            else
-            {
-                m_clearedAnim.SetActive(false);
+                m_clearedAnim[i].SetActive(m_clearedAnim[i]);
             }
         }
     }
